3ad0cf457bc317a7708d97db4a0fd54db7d6ccf3
sbhta <sbhta12345@gmail.com>
2025-06-05 21:07:06 +0300

    FINALLY FIXED FONT ISSUE AND LOADING NERDFONT ICONS

--- src/fonts.cpp
+++ src/fonts.cpp
@@ -2,17 +2,24 @@
 #include "raylib.h"
 #include <stdexcept>
 #include <string>
+#include <vector>
 
 FontManager::~FontManager(){
    unloadAll();
 }
 
 void FontManager::loadFont(const std::string &name, const std::string &filePath, int size){
-   if (fonts[name].count(size) > 0) return;
-   Font font = LoadFontEx(filePath.c_str(), size, nullptr, 0);
+   if (fonts[name].count(size) > 0) return; // font already loaded
+
+   std::vector<int> codepoints = {0xf02c2};
+   for (int i = 0x20; i <= 0x7E; i++) codepoints.push_back(i);
+   for (int i = 0xA0; i <= 0xFF; i++) codepoints.push_back(i);
+   // between 0xE000 and 0x10FFFF if you want to have access to ALL nerdfont icons and what not
+   Font font = LoadFontEx(filePath.c_str(), size, codepoints.data(), codepoints.size());
    if (font.texture.id == 0) {
       throw std::runtime_error("Failed to load font: " + filePath);
    }
+
    fonts[name][size] = font;
 }
 
